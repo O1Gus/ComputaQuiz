4a469f101abde42d6fa792d61f0b2eed42a56f3b
O1Gus <gustavorf106@gmail.com>
2025-10-24 21:11:31 -0300

    teste login google 1.4

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: true,   // com HTTPS
+    secure: false,   // <<<<< ALTERAR PARA FALSE
     httpOnly: true,
     sameSite: 'lax',
     maxAge: 24*60*60*1000
